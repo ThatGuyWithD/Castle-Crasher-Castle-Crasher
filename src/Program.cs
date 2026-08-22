877e68a2836bea21984e434a176e7cc4f5cfb39e
3AZYE <126979239+ThatGuyWithD@users.noreply.github.com>
2026-08-21 22:26:03 -0700

    Add application entry point

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
+    }
+}
